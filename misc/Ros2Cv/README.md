04d7b42e54777d1426527339dcf7428c8bfad1e9
kreolite <ssavrimo@gmail.com>
2016-11-24 14:28:31 +0000

    Add python ros node to perform opencv processing

---
+++ misc/Ros2Cv/README.md
@@ -0,0 +1,18 @@
+This node aims at converting Compressed image from Android application to Opecn image.
+It will then try to track a green ball.
+
+To do that:
+- Install docker then pull osrf/ros:indigo-desktop-full images
+- Launch this image
+- docker run -it -P -p11311:11311 --env="ROS_IP=172.17.0.2" --env="ROS_MASTER_URI=http://ip_of_master:11311" --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" osrf/ros:indigo-desktop-full bash
+- rosrun image_transport republish compressed in:=/camera/image _image_t_transport:=compressedut:=camera2
+- python balltracker.py
+
+Reference:
+
+http://www.pyimagesearch.com/2015/09/21/opencv-track-object-movement/
+http://wiki.ros.org/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
+
+
+TODO:
+Make a docker file
---
+++ misc/Ros2Cv/balltracker.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import roslib
+roslib.load_manifest('my_package')
+import sys
+import rospy
+import cv2
+import numpy as np
+import imutils
+from collections import deque
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space, then initialize the
+# list of tracked points
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
+BUFFER_SIZE = 10
+pts = deque(maxlen=BUFFER_SIZE)
+
+
+def process_image(frame):
+    # resize the frame, blur it, and convert it to the HSV
+    # color space
+    frame = imutils.resize(frame, width=600)
+    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # construct a mask for the color "green", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        # only proceed if the radius meets a minimum size
+        if radius > 10:
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+    # update the points queue
+    pts.appendleft(center)
+
+    # loop over the set of tracked points
+    for i in xrange(1, len(pts)):
+        # if either of the tracked points are None, ignore
+        # them
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+
+        # otherwise, compute the thickness of the line and
+        # draw the connecting lines
+        thickness = int(np.sqrt(BUFFER_SIZE / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+    # show the frame to our screen
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+
+class image_converter:
+
+    def __init__(self):
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("camera2",Image,self.callback)
+
+    def callback(self,data):
+        try:
+          cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+          print(e)
+        process_image(cv_image)
+
+def main(args):
+    ic = image_converter()
+    rospy.init_node('image_converter', anonymous=True)
+    try:
+       rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
