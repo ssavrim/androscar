160ebdf6b5d149fbc0e05ae6ffed460e500f5216
kreolite <ssavrimo@gmail.com>
2016-12-05 16:02:31 +0100

    First implementation to aggregate data from sonar and car command

--- misc/RosNodes/selfdriving/selfdriving.py
+++ misc/RosNodes/selfdriving/selfdriving.py
@@ -5,18 +5,35 @@ roslib.load_manifest('cv_bridge')
 import sys
 import rospy
 from sensor_msgs.msg import Range
+from geometry_msgs.msg import Twist
 
-class sensor_listener:
+_last_left = 0.0
+_last_right = 0.0
+_last_x = 0.0
+_last_z = 0.0
 
-    def __init__(self):
-        self.sonar_sub = rospy.Subscriber("car_sensor/front_range",Range,self.callback)
+def sonar_callback(data):
+    global _last_left, _last_right, _last_x, _last_z
+    if data.range == 0:
+        return
+    if data.header.frame_id == "/left":
+        _last_left = data.range
+    else:
+        _last_right = data.range
+    if _last_right <= 10 or _last_left <= 10 :
+        rospy.logwarn("ALERT !")
+    rospy.loginfo([_last_left, _last_right, _last_x, _last_z])
 
-    def callback(self,data):
-        print(data)
+
+def command_callback(data):
+    global _last_x, _last_z
+    _last_x = data.linear.x
+    _last_z = data.angular.z
 
 def main(args):
-    ic = sensor_listener()
     rospy.init_node('sensor_listener', anonymous=True)
+    rospy.Subscriber("car_sensor/front_range", Range, sonar_callback)
+    rospy.Subscriber("car_command/cmd_vel", Twist, command_callback)
     try:
        rospy.spin()
     except KeyboardInterrupt:
