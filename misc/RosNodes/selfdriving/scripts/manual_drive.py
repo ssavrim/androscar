db7d16ffca556161bb8bd3197f9aadcde0bcf9ad
kreolite <ssavrimo@gmail.com>
2017-02-20 10:03:53 +0100

    Update script to control the car from pygame

--- misc/RosNodes/selfdriving/scripts/manual_drive.py
+++ misc/RosNodes/selfdriving/scripts/manual_drive.py
@@ -15,13 +15,13 @@ class ManualDrive(object):
 
     def __init__(self):
         self._command_publisher = None
-        self._linear_x = 0.0
-        self._angular_z = 0.0
+        self._throttle = 0.0
+        self._steering = 0.0
 
     def init(self):
         pygame.display.init()
-        pygame.display.set_mode((320, 240))
-        # Init publisher to send predicted command
+        pygame.display.set_mode((640, 480))
+        pygame.key.set_repeat(500, 30)
         rospy.init_node(self.NODE_NAME, anonymous=True)
         self._command_publisher = rospy.Publisher(constants.VELOCITY_ACTION_TOPIC, Twist, queue_size=10)
 
@@ -32,24 +32,26 @@ class ManualDrive(object):
                     if event.type == pygame.KEYDOWN:
                         keys=pygame.key.get_pressed()
                         if keys[pygame.K_LEFT]:
-                            print('LEFT')
-                            self._angular_z += 0.1
-                            self._angular_z  = min(self._angular_z, 1.0)
+                            print("Go left")
+                            self._steering += 0.1
+                            self._steering = min(self._steering, 1.0)
                         if keys[pygame.K_RIGHT]:
-                            print('RIGHT')
-                            self._angular_z -= 0.1
-                            self._angular_z  = max(self._angular_z, -1.0)
+                            print("Go right")
+                            self._steering -= 0.1
+                            self._steering = max(self._steering, -1.0)
                         if keys[pygame.K_UP]:
-                            print('FORWARD')
-                            self._linear_x = 1.0
+                            print("Go forward")
+                            self._throttle = 1.0
+                            self._steering = 0.0
                         if keys[pygame.K_DOWN]:
-                            print('BACKWARD')
-                            self._linear_x = -1.0
+                            print("Go backward")
+                            self._throttle = -1.0
+                            self._steering = 0.0
                         if keys[pygame.K_SPACE]:
-                            print('STOP')
-                            self._linear_x = 0.0
-                            self._angular_z = 0.0
-                        cmd_vel = Twist(Vector3(self._linear_x, 0, 0), Vector3(0, 0, self._angular_z))
+                            print("Stop")
+                            self._throttle = 0.0
+                            self._steering = 0.0
+                        cmd_vel = Twist(Vector3(self._throttle, 0, 0), Vector3(0, 0, self._steering))
                         self._command_publisher.publish(cmd_vel)
 
         else:
